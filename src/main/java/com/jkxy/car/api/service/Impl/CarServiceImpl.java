c77754876a98bcac3629bbd23ddceb608c9eafd8
skm2d3 <Youfeng1_Zhu@saic-gm.com>
2020-05-18 09:51:40 +0800

    增加事务处理

--- src/main/java/com/jkxy/car/api/service/Impl/CarServiceImpl.java
+++ src/main/java/com/jkxy/car/api/service/Impl/CarServiceImpl.java
@@ -7,6 +7,8 @@ import com.jkxy.car.api.pojo.CarSell;
 import com.jkxy.car.api.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Timestamp;
 import java.util.List;
@@ -51,6 +53,7 @@ public class CarServiceImpl implements CarService {
     }
 
     @Override
+    @Transactional(isolation = Isolation.SERIALIZABLE)
     public String buyCar(int id, int amount) {
         if(amount<1){
             return "购买数量异常";
